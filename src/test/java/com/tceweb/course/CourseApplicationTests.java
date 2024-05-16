f1096b0da60103fefe3631e77bca121bd097d0cb
Luiz Henrique <luizh.rocha16@gmail.com>
2024-05-16 10:58:50 -0300

    Project Created

---
+++ src/test/java/com/tceweb/course/CourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tceweb.course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
