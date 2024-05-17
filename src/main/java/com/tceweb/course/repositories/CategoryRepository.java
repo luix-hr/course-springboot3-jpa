8ff33999b23b714d733111db939def9585964354
Luiz Henrique <luizh.rocha16@gmail.com>
2024-05-17 10:29:31 -0300

    Category

---
+++ src/main/java/com/tceweb/course/repositories/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.tceweb.course.repositories;
+
+import com.tceweb.course.entities.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+
+}
