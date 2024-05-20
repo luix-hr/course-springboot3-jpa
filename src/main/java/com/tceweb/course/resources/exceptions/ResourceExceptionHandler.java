74b6132892e703784f4006a13a2eaa16719f778b
Luiz Henrique <luizh.rocha16@gmail.com>
2024-05-20 09:54:08 -0300

    Exception handling - delete

--- src/main/java/com/tceweb/course/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/tceweb/course/resources/exceptions/ResourceExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.tceweb.course.resources.exceptions;
 
+import com.tceweb.course.services.exceptions.DatabaseException;
 import com.tceweb.course.services.exceptions.ResourceNotFoundException;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
@@ -17,6 +18,16 @@ public class ResourceExceptionHandler {
 
         String error = "Resource not found";
         HttpStatus status = HttpStatus.NOT_FOUND;
+        StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(status).body(err);
+
+    }
+
+    @ExceptionHandler(DatabaseException.class)
+    public ResponseEntity<StandardError> dataBaseException(DatabaseException e, HttpServletRequest request){
+
+        String error = "Database error";
+        HttpStatus status = HttpStatus.BAD_REQUEST;
         StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
         return ResponseEntity.status(status).body(err);
 
